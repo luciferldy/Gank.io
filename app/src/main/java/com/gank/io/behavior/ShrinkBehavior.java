84862159f3f90723696dc4c2d4dd3cdb271ed54a
luciferldy <2460572865@qq.com>
2017-03-10 16:49:34 +0800

    fix

--- app/src/main/java/com/gank/io/behavior/ShrinkBehavior.java
+++ app/src/main/java/com/gank/io/behavior/ShrinkBehavior.java
@@ -8,13 +8,18 @@ import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.gank.io.util.Logger;
+
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by lian_ on 2017/3/10.
  */
 
 public class ShrinkBehavior extends CoordinatorLayout.Behavior<FloatingActionButton> {
+
+    private static final String LOG_TAG = ShrinkBehavior.class.getSimpleName();
 
     public ShrinkBehavior() {
         super();
@@ -31,8 +36,11 @@ public class ShrinkBehavior extends CoordinatorLayout.Behavior<FloatingActionBut
 
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
-        float translationY = getFabTranslationYForSnackbar(parent, child);
-        float percentComplete = -translationY / dependency.getHeight();
+//        Logger.i(LOG_TAG, String.format(Locale.CHINA, "onDependentChanged dependency getTranslationY %f, getHeight %d", ViewCompat.getTranslationY(dependency), dependency.getHeight()));
+//        float translationY = getFabTranslationYForSnackbar(parent, child);
+//        float percentComplete = -translationY / dependency.getHeight();
+        float translationY = dependency.getHeight() - ViewCompat.getTranslationY(dependency);
+        float percentComplete = translationY / dependency.getHeight();
         float scaleFactor = 1-percentComplete;
 
         child.setScaleX(scaleFactor);
@@ -40,13 +48,20 @@ public class ShrinkBehavior extends CoordinatorLayout.Behavior<FloatingActionBut
         return false;
     }
 
+    /**
+     * 这个方法貌似可以不用，如果遇到问题再解决
+     * @param parent CoordinateLayout
+     * @param fab FloatingActionButton
+     * @return offset
+     */
+    @Deprecated
     private float getFabTranslationYForSnackbar(CoordinatorLayout parent, FloatingActionButton fab) {
         float minOffset = 0;
         final List<View> dependencies = parent.getDependencies(fab);
         for (int i = 0, z = dependencies.size(); i < z; i++) {
             final View view = dependencies.get(i);
             if (view instanceof Snackbar.SnackbarLayout && parent.doViewsOverlap(fab, view)) {
-                minOffset = Math.min(minOffset, ViewCompat.getTranslationY(view));
+                minOffset = Math.min(minOffset, ViewCompat.getTranslationY(view) - view.getHeight());
             }
         }
         return minOffset;
